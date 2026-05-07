1a2dd4fc51b4c2eb295a9e4eaa604f090a043365
osamuelelvis <elvislimasamuel27@gmail.com>
2026-05-07 16:58:19 -0300

    added part 2 vetorized exercise

---
+++ expt2vetor1.py
@@ -0,0 +1,33 @@
+import platform
+import subprocess
+
+def get_so():
+    return platform.system()
+
+def ping():
+    so = get_so()
+    if so == 'Windows':
+        print("Esse PC é um Windows")
+        comando = 'ping -4 -n 10 www.google.com.br' 
+    else:
+        print("Esse PC é um Linux")
+        comando = 'ping -4 -c 10 www.google.com.br'
+
+    resultado = subprocess.run(comando, capture_output=True, text=True, shell=True)
+    linhas = resultado.stdout.split('\n')
+    ultima_linha = linhas[-2]
+
+    if so == 'Windows':
+        partes = ultima_linha.split(', ')
+        media = partes[2].split(' = ')[1]
+    else:
+        partes = ultima_linha.split(' ')
+        media = partes[3].split('=')[1].split('/')[1]
+
+    print(f"Média de ping: {media}")
+
+def main():
+    ping()
+
+if __name__ == '__main__':
+    main()
